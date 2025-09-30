8e031cc843de1422a7d3360ab676975bea539e09
ahmed123 <mm1869695@gmail.com>
2025-10-01 02:01:22 +0300

    First Commit

---
+++ src/main/java/com/mycompany/firstapp/FirstApp.java
@@ -0,0 +1,14 @@
+ 
+package com.mycompany.firstapp;
+public class FirstApp {
+    public static void main(String[] args) {
+        Person _myPerson=new Person("Red",3.0,0.5,true);
+        _myPerson.PrintProps();
+        System.out.println("Hello World!");
+    }
+    
+
+}
+
+
+
---
+++ src/main/java/com/mycompany/firstapp/Person.java
@@ -0,0 +1,40 @@
+package com.mycompany.firstapp;
+public class Person {
+    
+    /*
+        Class Props
+    */
+    public String Color;  // سلسه نصيه    ahmed alaa  // ""
+    public double Weight; // 7.45  // 0
+    public double Length;
+    public boolean IsMale;  // false   // true or false
+    
+    /*
+        Class Constuctor
+    */  
+     // constructor البناء 
+    // defult constructor    فى حاله انى معملتش بناء
+    public Person(String _color,double weight,double lenght,boolean _ismale)
+    {
+        this.Color=_color;
+        this.Weight=weight;
+        this.Length=lenght;
+        this.IsMale=_ismale;
+        
+        
+    }
+     
+    
+    /*
+        class Methods And Functions
+    */
+    public void PrintProps()
+    {
+        System.out.println("My Color Is :"+this.Color);
+        System.out.println("My Weight Is :"+this.Weight); 
+        System.out.println("My Length Is :"+this.Length);
+        System.out.println("My IsMale Is :"+this.IsMale);
+    }
+    
+    
+}
